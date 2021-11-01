43b36a65c11f2dd974592c965a6428c95a772467
Olesia <olesyao2003@gmail.com>
2021-11-01 21:16:30 +0200

    Added get_words()

---
+++ target_game.py
@@ -0,0 +1,79 @@
+"""Target game"""
+from typing import List
+import random
+
+
+def generate_grid() -> List[List[str]]:
+    """
+    Generates list of lists of letters - i.e. grid for the game.
+    e.g. [['I', 'G', 'E'], ['P', 'I', 'S'], ['W', 'M', 'G']]
+    """
+    letters_list = []
+
+    for i in range(9):
+        letter_ord = random.randint(97, 122)
+        letters_list.append(chr(letter_ord))
+
+
+    return letters_list
+
+
+def get_words(f: str, letters: List[str]) -> List[str]:
+    """
+    Reads the file f. Checks the words with rules and returns a list of words.
+    """
+    # letters_tuples = []
+    # grid_list = []
+    # for letter in letters_list:
+    #     letters_tuples.append((letter, letters_list.count(letter)))
+    # for letter in letters_tuples:
+    #     if letter not in grid_list:
+    #         grid_list.append(letter)
+    words_from_dict = []
+
+    with open(f, 'r') as file:
+        content = file.read()
+    content = content.lower()
+    all_words = content.split()
+    del all_words[0:4]
+
+    central_letter = letters[4]
+    for word in all_words:
+        if (len(word) >= 4) and (word.count(central_letter) >= 1):
+            num = 0
+            for letter in word:
+                if letters.count(letter) >= 1:
+                    num += 1
+            if num == len(word):
+                num = 0
+                for letter in word:
+                    if word.count(letter) <= letters.count(letter):
+                        num += 1
+                if num == len(word):
+                    words_from_dict.append(word)
+    return words_from_dict
+
+print(get_words('/home/olesia/UCU_OP/Year_1/Lab_6/game_lab_6/en.txt', [el for el in 'jniarnoah']))
+        
+
+
+def get_user_words() -> List[str]:
+    """
+    Gets words from user input and returns a list with these words.
+    Usage: enter a word or press ctrl+d to finish.
+    """
+    pass
+
+
+def get_pure_user_words(user_words: List[str], letters: List[str], words_from_dict: List[str]) -> List[str]:
+    """
+    (list, list, list) -> list
+
+    Checks user words with the rules and returns list of those words
+    that are not in dictionary.
+    """
+    pass
+
+
+def results():
+    pass
